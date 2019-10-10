376d3ead01f7a4be5e910abd4eb245a192e6b202
Victoria Hale <vic.hale74@gmail.com>
2019-10-10 01:22:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def now_serving(array)
     puts "There is nobody waiting to be served!"
   else 
     
- puts "Welcome, #{name}. You are number #{array.length} in line." 
+ puts "Currently serving #{name}." 
   end
 end
   
