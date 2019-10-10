967ab971c83bfa897d8f2f51c42275054f0aa8ad
Victoria Hale <vic.hale74@gmail.com>
2019-10-10 01:14:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,6 @@ end
 
 def take_a_number(katz_deli, name)
   
-   katz_deli.each.with_index(1) do |name, index|
    katz_deli.push("#{index}. #{name}")
  puts "Welcome, #{name}. You are number #{index} in line." # puts name of person and their position in line
 end
