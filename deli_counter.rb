9327c71546322b48c72955487ee7606b3791ae69
Victoria Hale <vic.hale74@gmail.com>
2019-10-10 00:49:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(place) # Write your code here.
+new_array = []
+  if place.length == 0 
+    puts "The line is currently empty."
+  else  
+    place.each.with_index(1) do |name, index|
+      new_array.push("#{index}. #{name}") 
+  end
+    puts "The line is currently: #{new_array.join(" ")}"
+  end
+end
+
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  
+end
